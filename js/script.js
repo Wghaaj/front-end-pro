e6d0c0102531efea8c48fcdaaf69a2da4052e47d
Diana Kravchuk <dianakravchuk@Dianas-MacBook-Air.local>
2024-06-20 10:39:26 -0600

    some

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const input1 = prompt('Введіть перший рядок:');
 const input2 = prompt('Введіть другий рядок:');
 const input3 = prompt('Введіть третій рядок:');
-console.log(`Ви ввели: "${input2}", "${input1}", "${input3}"`);
+console.log(`Ви ввели: ${input2}, ${input1}, ${input3}`);
+console.log("Ви ввели:" + input1 + " " + input2 + " " + input3)
