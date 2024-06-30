fc2bc092113a56f57720760193af34b751332932
Diana Kravchuk <dianakravchuk@Dianas-MacBook-Air.local>
2024-06-30 17:18:07 -0600

    added changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
-const array = ['hello', 35, 78, 'ok', 98, 1, 'pizza']
-const filtered = array.filter((num) => typeof num === 'number')
-// console.log(filtered)
-const sum = filtered.reduce((a, b) => a + b, 0)
-// console.log(sum)
-const average  = sum / filtered.length
-console.log(average)
+let input = prompt('Enter your values seperated by commas')
+// разделить данные на массив и изменить на числа где потребуется 
+let array = input.split(',').map(item => {
+    item = item.trim()
+    return isNaN(item) ? item : Number(item)
+})
+// console.log(array)
+
+function findAverage(array) {
+    let filtered = array.filter((num) => typeof num === 'number')
+    // console.log(filtered)
+    let sum = filtered.reduce((a, b) => a + b, 0)
+    // console.log(sum)
+    let average  = sum / filtered.length
+    console.log(average)
+}
+findAverage(array)
+
