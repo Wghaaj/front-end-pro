d4da273688b0bf1f4e88b31bb3fb1b01ba3b5fff
Diana Kravchuk <dianakravchuk@Dianas-MacBook-Air.local>
2024-07-03 22:25:34 -0600

    added hw7.1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function createSum() {
+    let total = 0;
+    
+    return function(sum) {
+      total += sum;
+      return total;
+    }
+  }
+  
+  const sum = createSum();
+  
+  console.log(sum(4)); 
+  console.log(sum(6)); 
+  console.log(sum(10));
+  console.log(sum(7)); 
+  
