3e9a9cc22b4f8b98e5d5887025d67cdfc26d1a11
Diana Kravchuk <dianakravchuk@Dianas-MacBook-Air.local>
2024-07-03 22:40:34 -0600

    added hw7.3

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function promptForNumber() {
+    let number = null;
+  
+    for (let i = 0; i < 10; i++) {
+      number = prompt("Введіть число більше 100:");
+  
+      if (number === null) {
+        // Користувач натиснув "Скасувати"
+        break;
+      }
+  
+      number = Number(number);
+  
+      if (number > 100) {
+        break;
+      }
+    }
+  
+    console.log(`Останнє введене число: ${number}`);
+  }
+  
+  promptForNumber();
