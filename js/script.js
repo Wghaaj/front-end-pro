99709dfb6a7c7be11d3e8e91edd98a26b6bb81b8
Diana Kravchuk <dianakravchuk@Dianas-MacBook-Air.local>
2024-08-18 18:19:38 -0600

    added hw10.2

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+let filtered = numbers.filter((num) => num % 2 === 0)
+console.log('filtered array:', filtered);
